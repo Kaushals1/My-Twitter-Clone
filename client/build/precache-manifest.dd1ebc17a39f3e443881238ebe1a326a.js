c6a732db34bd7773b41c1622915c9859c3be0347
Kaushals1 <kaushalsheth50@gmail.com>
2020-09-22 20:32:52 +0530

    build

---
+++ client/build/precache-manifest.dd1ebc17a39f3e443881238ebe1a326a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58d135dc51afd1fccc10900db0b5c142",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d0a79a936cee12ba359",
+    "url": "/static/css/main.62fe28ed.chunk.css"
+  },
+  {
+    "revision": "2df489477439b30bf4dd",
+    "url": "/static/js/2.5afb6547.chunk.js"
+  },
+  {
+    "revision": "698c7d1ab7fc994ffbdcf6ff99e91ff0",
+    "url": "/static/js/2.5afb6547.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d0a79a936cee12ba359",
+    "url": "/static/js/main.71f019f0.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "4a773e98b1d02547830134bd39a200a2",
+    "url": "/static/media/t.4a773e98.png"
+  },
+  {
+    "revision": "4a70436a5d598c871042257fcd201178",
+    "url": "/static/media/twitter-social-media-emoji-3d-render-background-social-media-balloon-symbol_41204-1885.4a70436a.jpg"
+  }
+]);
